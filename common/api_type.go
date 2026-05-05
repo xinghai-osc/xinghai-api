c9b3369669161a9e3f9f758a785c142fe21bb1af
yeying-xingchen <yeying-xingchen@openatom.club>
2026-05-05 19:28:02 +0800

    fix: add ChannelType2APIType mapping for OpenAI2Claude channel

--- common/api_type.go
+++ common/api_type.go
@@ -75,6 +75,8 @@ func ChannelType2APIType(channelType int) (int, bool) {
 		apiType = constant.APITypeReplicate
 	case constant.ChannelTypeCodex:
 		apiType = constant.APITypeCodex
+	case constant.ChannelTypeOpenAI2Claude:
+		apiType = constant.APITypeOpenAI2Claude
 	}
 	if apiType == -1 {
 		return constant.APITypeOpenAI, false
