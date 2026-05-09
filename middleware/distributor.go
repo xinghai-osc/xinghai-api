0edb2388cd388b762cc047981c68831cc8283adf
yeying-xingchen <yeying-xingchen@openatom.club>
2026-05-09 20:09:58 +0800

    fix: playground 403

--- middleware/distributor.go
+++ middleware/distributor.go
@@ -90,7 +90,7 @@ func Distribute() func(c *gin.Context) {
 						return
 					}
 					if playgroundRequest.Group != "" {
-						if !service.GroupInUserUsableGroups(usingGroup, playgroundRequest.Group) && playgroundRequest.Group != usingGroup {
+						if playgroundRequest.Group != "auto" && !service.GroupInUserUsableGroups(usingGroup, playgroundRequest.Group) && playgroundRequest.Group != usingGroup {
 							abortWithOpenAiMessage(c, http.StatusForbidden, i18n.T(c, i18n.MsgDistributorGroupAccessDenied))
 							return
 						}
