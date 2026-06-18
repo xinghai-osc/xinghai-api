b82947efed47dd097d1c4d9b21ea87eae3c0fb64
yeying-xingchen <yeying-xingchen@openatom.club>
2026-06-18 15:56:40 +0800

    fix(ui): show user agent in usage log details

--- web/default/src/features/usage-logs/components/dialogs/details-dialog.tsx
+++ web/default/src/features/usage-logs/components/dialogs/details-dialog.tsx
@@ -625,6 +625,14 @@ export function DetailsDialog(props: DetailsDialogProps) {
             />
           )}
 
+          {props.log.user_agent && (
+            <DetailRow
+              label={t('User Agent')}
+              value={props.log.user_agent}
+              mono
+            />
+          )}
+
           {showTiming && props.log.use_time > 0 && (
             <DetailRow
               label={t('Response Time')}
